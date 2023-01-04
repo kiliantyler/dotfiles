e172ca9f31d56127f94d2be3df3392b4b62152ea
Kilian Tyler <kilian@ktyler.me>
2023-01-03 19:17:59 -0500

    sops

--- oh-my-zsh/.oh-my-zsh/custom/03_exports.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/03_exports.zsh
@@ -3,3 +3,4 @@ export ZSH_PLUGINS_ALIAS_TIPS_TEXT="Alias: "
 export ZSH_PLUGINS_ALIAS_TIPS_EXCLUDES="_"
 export MANPATH="/usr/local/man:$MANPATH"
 export KUBECONFIG=$(python3 -c "import glob;print(':'.join(glob.glob('$HOME/.kube/config-files/*.yaml')))")
+export SOPS_AGE_KEY_FILE="${HOME}/.config/sops/age/keys.txt"
