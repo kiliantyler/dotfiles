6f2e8276e74c09459cb83661e80c1d1ed02c169c
Kilian Tyler <kilian@kil.dev>
2024-12-31 21:20:24 -0500

    git commits broken

--- dotfiles/private_dot_config/private_dot_gitconfig/template/configure.sh
+++ dotfiles/private_dot_config/private_dot_gitconfig/template/configure.sh
@@ -1,24 +1,24 @@
-#!/usr/bin/env bash
+# #!/usr/bin/env bash
 
-readonly PROGNAME=$(basename "$0")
-readonly PROGDIR="$(
-  cd "$(dirname "$0")"
-  pwd
-)"
-readonly GIT_COMMON_DIR="$(git rev-parse --git-common-dir)"
-export GIT_COMMON_DIR
+# readonly PROGNAME=$(basename "$0")
+# readonly PROGDIR="$(
+#   cd "$(dirname "$0")"
+#   pwd
+# )"
+# readonly GIT_COMMON_DIR="$(git rev-parse --git-common-dir)"
+# export GIT_COMMON_DIR
 
-. "$GIT_COMMON_DIR/hooks/hook-switcher"
+# . "$GIT_COMMON_DIR/hooks/hook-switcher"
 
-main() {
-  local hookName
-  for directory in $(find "$PROGDIR/hooks" -type d | sort |
-    awk '$0 !~ last "/" {print last} {last=$0} END {print last}'); do
-    hookName=${directory#$PROGDIR/hooks/}
-    echo "Configuring $hookName"
-    switch_hook "$hookName"
-    find "$directory" -name configure.sh -exec {} \;
-  done
-}
+# main() {
+#   local hookName
+#   for directory in $(find "$PROGDIR/hooks" -type d | sort |
+#     awk '$0 !~ last "/" {print last} {last=$0} END {print last}'); do
+#     hookName=${directory#$PROGDIR/hooks/}
+#     echo "Configuring $hookName"
+#     switch_hook "$hookName"
+#     find "$directory" -name configure.sh -exec {} \;
+#   done
+# }
 
-main
+# main
